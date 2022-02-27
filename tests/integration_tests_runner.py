da4549e4432caf175291a82cfbcf67f198fc7b3a
oliverpena <oliverpena.itt@gmail.com>
2022-02-27 15:34:50 -0400

    initial commit.

---
+++ tests/integration_tests_runner.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import unittest
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().discover(
+        start_dir='test/integration_tests', pattern='*test.py')
+    resutl = unittest.TextTestRunner(verbosity=2).run(suite)
+    if resutl.wasSuccessful():
+        try:
+            sys.exit()
+        except SystemExit:
+            os._exit(0)
+        except Exception:
+            print('Error while running tests.')
+    else:
+        try:
+            sys.exit(1)
+        except SystemExit:
+            os._exit(1)
